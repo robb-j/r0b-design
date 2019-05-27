a61e332b5d05a210ee5155a0d6c40678cb9eef60
Rob Anderson <rob@andrsn.uk>
2019-05-27 19:12:01 +0100

    :bug: Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,4 @@ ssh r0b rm -r /srv/apps/files/ux
 scp ux.tar.gz r0b:/srv/apps/files/ux.tar.gz
 ssh r0b tar -C /srv/apps/files -xzf /srv/apps/files/ux.tar.gz
 ssh r0b mv /srv/apps/files/dist /srv/apps/files/ux
+ssh r0b chmod -R +r /srv/apps/files/ux
