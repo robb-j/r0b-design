329a7a072df15fa321478118bb84655b27bbe49d
Rob Anderson <rob@andrsn.uk>
2019-05-29 22:36:14 +0100

    :memo: Add StopWatch docs

--- cli/stop-watch.js
+++ cli/stop-watch.js
@@ -1,15 +1,19 @@
 const ms = require('ms')
 
+/** A utility to time a series of events and output them to the console */
 class StopWatch {
+  /** Creates a time and log the initial time */
   constructor() {
     this.startedAt = Date.now()
     this.timeseries = []
   }
 
+  /** Record an event */
   record(title) {
     this.timeseries.push({ title, time: Date.now() })
   }
 
+  /** Output all events with relative durations */
   dump() {
     let currentTime = this.startedAt
 
